ed1fc0bc8632fa5a1c5fde59166413a4a70b50f5
Juan <rigualjuan@gmail.com>
2023-02-10 14:50:05 -0300

    add todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { FormContextProvider } from "./context/FormContext";
 import CardForm from "./pages/CardForm";
 
+/**
+ * TODO
+ * VALIDAR FORM
+ * AGREGAR ESTADOS DE ERROR
+ * RESTRINGIR CARACTERES DE NUMEROS / LETRAS RESPECTIVAMENTE
+ */
 function App() {
   return (
     <FormContextProvider>
--- src/components/ui/card-ui/CardUI.tsx
+++ src/components/ui/card-ui/CardUI.tsx
@@ -2,10 +2,6 @@ import { BaseComponentInterface } from "../../../interfaces/component-base";
 import { BasePadding } from "../../../styles/base-padding";
 import { CardUIStyles } from "./card-ui.styles";
 
-/**
- * TODO
- * FIX STYLES ON CARD
- */
 const CardUI = ({ children }: BaseComponentInterface) => (
   <CardUIStyles>
     <BasePadding>{children}</BasePadding>
